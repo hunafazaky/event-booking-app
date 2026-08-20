7684f569005ed644741f41765223e19bda733f6d
hunafazaky <hunafa.zsn@gmail.com>
2026-08-20 22:25:33 +0700

    new: add event repository

---
+++ internal/repository/event_repository.go
@@ -0,0 +1,87 @@
+package repository
+
+import (
+	"github.com/hunafazaky/event-booking-app/internal/model"
+	"gorm.io/gorm"
+)
+
+type EventRepository interface {
+	Create(event *model.Event) error
+	Update(event *model.Event) error
+	Delete(event *model.Event) error
+	FindAll(search string, page, limit int) (events []model.Event, totalRows, totalPages int64, err error)
+	FindByID(id uint) (*model.Event, error)
+	FindByUserID(userID uint) (events []model.Event, err error)
+}
+
+type eventRepository struct {
+	db *gorm.DB
+}
+
+func NewEventRepository(db *gorm.DB) EventRepository {
+	return &eventRepository{db: db}
+}
+
+func userSummary(db *gorm.DB) *gorm.DB {
+	return db.Select("id", "name", "email")
+}
+
+func (r *eventRepository) Create(event *model.Event) error {
+	return r.db.Create(event).Error
+}
+
+func (r *eventRepository) Update(event *model.Event) error {
+	return r.db.Save(event).Error
+}
+
+func (r *eventRepository) Delete(event *model.Event) error {
+	return r.db.Delete(event).Error
+}
+
+func (r *eventRepository) FindAll(search string, page, limit int) (events []model.Event, totalRows, totalPages int64, err error) {
+
+	// Initialize Query
+	query := r.db.Model(&model.Event{})
+	if search != "" {
+		query = query.Where("name ILIKE ? OR description ILIKE ?", "%"+search+"%", "%"+search+"%")
+	}
+
+	// Total Rows
+	query.Count(&totalRows)
+
+	// Total Pages
+	if page < 1 {
+		page = 1
+	}
+	if limit < 1 {
+		limit = 6
+	}
+	totalPages = (totalRows + int64(limit) - 1) / int64(limit)
+
+	// Event & Error
+	offset := (page - 1) * limit
+	err = query.
+		Preload("User", userSummary).
+		Limit(limit).
+		Offset(offset).
+		Find(&events).Error
+	return
+}
+
+func (r *eventRepository) FindByID(id uint) (*model.Event, error) {
+	var event model.Event
+	err := r.db.
+		Preload("User", userSummary).
+		Preload("Booking").
+		Preload("Booking.User", userSummary).
+		First(&event, id).Error
+	return &event, err
+}
+
+func (r *eventRepository) FindByUserID(userID uint) (events []model.Event, err error) {
+	err = r.db.
+		Preload("User", userSummary).
+		Where("user_id", userID).
+		Find(&events).Error
+	return
+}
